d5b68403e1d98f53eb2e3226ea6cb0e4b66933a4
AnshKadluni <ansh_k@DESKTOP-ONMV9GP>
2025-03-18 10:10:29 +1030

    cool

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,6 +1,7 @@
 #include "Reverser.h"
 
 int Reverser::reverseDigit(int value) {
+    std::cout << value << std::endl;
     if (log10(value) <= 1) return value%10;
     return (value%10)*(pow(10, floor(log10(value)))) + reverseDigit(static_cast<int>(value/10));
 };
