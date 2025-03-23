423bedf7cf80d85e1d9eb0832e4bbe9555c386fb
AnshKadluni <ansh_k@DESKTOP-ONMV9GP>
2025-03-23 16:13:48 +1030

    cool

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -4,9 +4,6 @@ int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
     if (numCrates <= loadSize) return 1;
     if (memo.find(numCrates) != memo.end()) return memo[numCrates];
 
-    int half1 = numCrates/2;
-    int half2 = (numCrates % 2 == 0) ? half1 : half1 + 1;
-
     if (numCrates%2 == 0) {
 
         memo[numCrates] = numTrucks(static_cast<int>(numCrates/2), loadSize);
