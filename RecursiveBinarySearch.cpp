d94533570c73e52aebe418179755649c0971c691
Ansh Kaushal <a1945628@embg13w034.ad.adelaide.edu.au>
2025-04-09 12:57:49 +0930

    cool

--- RecursiveBinarySearch.cpp
+++ RecursiveBinarySearch.cpp
@@ -2,10 +2,12 @@
 
 bool RecursiveBinarySearch::search(std::vector<int> list, int mid) {
     if (list[mid] == 1) return true;
-    if (mid == )
+    if (low > high) return false;
     if (list[mid] > 1){
+        low = mid;
         search(list, (mid + list.size())/2);
     } else {
+        high = mid/2;
         search(list, mid/2);
     }
     
