9b1467cfbb6442630e4ccb0297328ea1c0a31088
AnshKadluni <ansh_k@DESKTOP-ONMV9GP>
2025-05-25 20:09:59 +0930

    done

--- klargest.cpp
+++ klargest.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int kth_largest(vector<int> values, int k) {
     priority_queue<int> pq(values.begin(), values.end());
     for (int i = 0; i < k; i++) {
-        if (pq.empty()) return numeric_limits<int>::max();
         pq.pop();
     }
 
